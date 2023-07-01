b44d33ef3611e576a6c63b0b1023f56218499fbe
Nikita Gupta <sanghinikita@gmail.com>
2023-07-01 15:44:47 +0530

    Portfolio Updated

--- src/components/git/Github.jsx
+++ src/components/git/Github.jsx
@@ -13,27 +13,57 @@ const GitHub = () => {
           <GitHubCalendar username="nikitaGupta2106" />
         </div>
 
+        {/* <div class="github-Stats">
+          <img
+            // id="github-streak-stats"
+            src="https://github-readme-streak-stats.herokuapp.com/?user=nikitaGupta2106"
+            alt="streaks"
+          />
+          <img
+            // id="github-stats-card"
+            src="https://github-readme-stats.vercel.app/api?username=nikitaGupta2106&show_icons=true"
+            alt="stats"
+          />
+          <img
+            // id="github-top-langs"
+            src="https://github-readme-stats.vercel.app/api/top-langs/?username=nikitaGupta2106&hide_progress=true"
+            alt="language"
+          />
+        </div> */}
+
         <div className="github__stats">
           <div className="github__stat" id="github-streak-stats">
-            <img src="" />
-            <iframe
+            {/* <iframe
               title="GitHub Streak"
               src="https://github-readme-streak-stats.herokuapp.com?user=nikitaGupta2106&theme=transparent"
-            ></iframe>
+            ></iframe> */}
+            <img
+              // id="github-streak-stats"
+              src="https://github-readme-streak-stats.herokuapp.com/?user=nikitaGupta2106"
+              alt="streaks"
+            />
           </div>
           <div className="github__stat" id="github-top-langs">
-            <img src="" />
-            <iframe
+            {/* <iframe
               title="GitHub Top Languages"
               src="https://github-readme-stats.vercel.app/api/top-langs/?username=nikitaGupta2106&theme=transparent"
-            ></iframe>
+            ></iframe> */}
+            <img
+              // id="github-stats-card"
+              src="https://github-readme-stats.vercel.app/api?username=nikitaGupta2106&show_icons=true"
+              alt="stats"
+            />
           </div>
           <div className="github__stat" id="github-stats-card">
-            <img src="" />
-            <iframe
+            {/* <iframe
               title="GitHub Stats"
               src="https://github-readme-stats.vercel.app/api?username=nikitaGupta2106&show_icons=true&&theme=transparent"
-            ></iframe>
+            ></iframe> */}
+            <img
+              // id="github-top-langs"
+              src="https://github-readme-stats.vercel.app/api/top-langs/?username=nikitaGupta2106&hide_progress=true"
+              alt="language"
+            />
           </div>
         </div>
       </div>
